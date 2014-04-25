49767770b157bafc138acb00d46ba4c867cf6623
shinnn <snnskwtnb@gmail.com>
2014-04-26 05:14:10 +0900

    introduce chalk and object-assign

--- tasks/merge_data.js
+++ tasks/merge_data.js
@@ -2,22 +2,16 @@
 // Copyright (c) 2013 - 2014 Shinnosuke Watanabe
 // Licensed under the MIT license
 
-module.exports = function (grunt) {
-  'use strict';
-  
-  var path = require('path');
-  
-  function mergeObjects (destObj, srcObj) {
-    for (var attrname in srcObj) {
-      if (srcObj.hasOwnProperty(attrname)) {
-        destObj[attrname] = srcObj[attrname];
-      }
-    }
-  }
-  
-  function mergeFileData (sources) {
+'use strict';
+
+var path = require('path');
+var chalk = require('chalk');
+var objectAssign = require('object-assign');
+
+module.exports = function(grunt) {
+  function mergeFileData(sources) {
     var data = {};
-    sources.filter(function (filePath) {
+    sources.filter(function(filePath) {
       // Warn on and remove invalid source files (if nonull was set)
       if (!grunt.file.exists(filePath)) {
         grunt.log.warn('Source file "' + filePath + '" not found.');
@@ -25,12 +19,11 @@ module.exports = function (grunt) {
       } else {
         return true;
       }
-
-    }).forEach(function (filePath) {
+    }).forEach(function(filePath) {
       var basename = path.basename(filePath, path.extname(filePath));
       var ext = path.extname(filePath).toLowerCase();
       
-      if(ext === '.json') {
+      if (ext === '.json') {
         data[basename] = grunt.file.readJSON(filePath);
       } else if (ext === '.yml' || ext === '.yaml') {
         data[basename] = grunt.file.readYAML(filePath);
@@ -46,63 +39,61 @@ module.exports = function (grunt) {
     return data;
   }
   
-  var mergeDataTask = function () {
-    // Merge task-specific and/or target-specific options with these defaults
-    var options = this.options({
-      data: null,
-      space: null,
-      asConfig: false
-    });
-    
-    // Iterate over all specified src/dest file groups
-    this.files.forEach(function (file) {
-      var data = mergeFileData(file.src);
-      
-      if (options.data) {
-        if (typeof options.data === 'function') {
-          mergeObjects(data, options.data(data));
-        } else {
-          mergeObjects(data, options.data);
-        }
-      }
-      
-      if (options.asConfig) {
-        var targetConfig;
-        if (typeof options.asConfig === 'string') {
-          targetConfig = options.asConfig;
-        
-        // Accept array of property name parts
-        } else if (Array.isArray(options.asConfig)) {
-          targetConfig = options.asConfig.join('.');
-          
-        } else if (options.asConfig === true) {
-          targetConfig = grunt.task.current.nameArgs
-                           .replace(':', '.') + '.context';
-        }
-        
-        grunt.config(targetConfig, data);
-        grunt.log.writeln('Config "' + targetConfig + '" updated.');
-      }
-
-      // Write the destination file if 'dest' is specified
-      if (file.dest) {
-        grunt.file.write(
-          file.dest,
-          JSON.stringify(data, null, options.space)
-        );
-        // Print a success message
-        grunt.log.writeln('File "' + file.dest + '" created.');
-
-      } else if (!options.asConfig) {
-        // when the task doesn't anything
-        grunt.log.warn('Neither destination path or config specified.');
-      }
-    });
-  };
-
   grunt.registerMultiTask(
     'merge_data',
-    'Merge multiple data into a file or Grint config.',
-    mergeDataTask
+    'Merge multiple data into a file or Grunt config.',
+    function() {
+      // Merge task-specific and/or target-specific options with these defaults
+      var options = this.options({
+        data: null,
+        space: null,
+        asConfig: false
+      });
+    
+      // Iterate over all specified src/dest file groups
+      this.files.forEach(function(file) {
+        var data = mergeFileData(file.src);
+      
+        if (options.data) {
+          if (typeof options.data === 'function') {
+            objectAssign(data, options.data(data));
+          } else {
+            objectAssign(data, options.data);
+          }
+        }
+      
+        if (options.asConfig) {
+          var targetConfig;
+          if (typeof options.asConfig === 'string') {
+            targetConfig = options.asConfig;
+        
+            // Accept array of property name parts
+          } else if (Array.isArray(options.asConfig)) {
+            targetConfig = options.asConfig.join('.');
+          
+          } else if (options.asConfig === true) {
+            targetConfig = grunt.task.current.nameArgs
+            .replace(':', '.') + '.context';
+          }
+        
+          grunt.config(targetConfig, data);
+          grunt.log.writeln('Config ' + chalk.green(targetConfig) + ' updated.');
+        }
+
+        // Write the destination file if 'dest' is specified
+        if (file.dest) {
+          grunt.file.write(
+            file.dest,
+            JSON.stringify(data, null, options.space)
+          );
+          // Print a success message
+          grunt.log.writeln('File ' + chalk.cyan(file.dest) + ' created.');
+
+        } else if (!options.asConfig) {
+          // when the task doesn't anything
+          grunt.log.warn('Neither destination path or config specified.');
+        }
+      });
+    }
   );
 };
